33c79db86d2c663aa6a6dad6796647ef8be5c378
ut-pro <utkarsh.singh144493@gmail.com>
2025-01-19 02:22:14 +0530

    simplify MongoDB connection options in main.js

--- main.js
+++ main.js
@@ -16,10 +16,7 @@ const port = 3000;
 
 app.listen(port, async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/inventory", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb://localhost:27017/inventory");
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
